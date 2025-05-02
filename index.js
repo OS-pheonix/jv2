ef50a83e604dc0d9fc10fde373a61a68f18ac906
OS-pheonix <longxjay@gmail.com>
2025-05-02 15:13:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
 // J.A.R.V.I.S Discord Bot Initialization
-// Last Updated: 2025-05-02 21:53:44
+// Last Updated: 2025-05-02 22:11:13
 // Author: OS-pheonix
 
 require('dotenv').config();
-const Jarvis = require('./jarvis.js');
+const client = require('./jarvis.js');
 
-const jarvis = new Jarvis();
-
-jarvis.login(process.env.DISCORD_TOKEN).then(() => {
-    console.log('J.A.R.V.I.S is initializing...');
-}).catch(error => {
-    console.error('Failed to start J.A.R.V.I.S:', error);
-});
-
-// Handle process termination
-process.on('SIGINT', () => {
-    console.log('J.A.R.V.I.S is shutting down...');
-    jarvis.destroy();
-    process.exit(0);
-});
-
-process.on('unhandledRejection', error => {
-    console.error('Unhandled promise rejection:', error);
-});
+// No additional code needed here as everything is handled in jarvis.js
