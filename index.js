e2b2d725b0acc7bfa18ef1d3d9317fa2ccd5ed76
OS-pheonix <longxjay@gmail.com>
2025-05-02 15:19:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 // J.A.R.V.I.S Discord Bot Initialization
-// Last Updated: 2025-05-02 22:11:13
+// Last Updated: 2025-05-02 22:18:31
 // Author: OS-pheonix
 
-require('dotenv').config();
 const client = require('./jarvis.js');
-
-// No additional code needed here as everything is handled in jarvis.js
